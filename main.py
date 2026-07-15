d7aae7a6b98b096daeff65757e03ce873b0b7b6b
Arthur Gomes <arthurgomees03@gmail.com>
2026-07-14 23:51:32 -0300

    chore: une em um arquivo

--- main.py
+++ main.py
@@ -1,5 +1,45 @@
-from fifo import executarFifo
-from lru import executarLru
+def executarFifo(quadros : int, referencia : list):
+    memoria = []
+    faltas = 0
+    
+
+    for pagina in referencia:
+        if (len(memoria) == quadros) :
+            if(pagina not in memoria):
+                faltas += 1
+                memoria.pop(0)
+                memoria.append(pagina)
+                    
+        else:
+            faltas +=1
+            memoria.append(pagina)
+            
+    return faltas
+
+
+def executarLru(quadros : int, referencia : list):
+    memoria = []
+    faltas = 0
+    
+
+    for paginaRef in referencia:
+        if (len(memoria) == quadros):
+            if (paginaRef in memoria):
+                for paginaMem in memoria:
+                    if paginaMem == paginaRef:
+                        memoria.remove(paginaMem)
+                        memoria.append(paginaMem)
+                        break
+            else:
+                faltas += 1
+                memoria.pop(0)
+                memoria.append(paginaRef)
+        else:
+            faltas +=1
+            memoria.append(paginaRef)
+
+    return faltas
+
 
 qQuadros = 0
 lReferencias = []
